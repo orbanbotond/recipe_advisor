547057008f801cd6ac129712c2c1d3672aa24d69
Orban Botond <orbanbotond@gmail.com>
2023-02-19 23:22:29 +0200

    Add comment for the query object

--- app/queries/relevant_recipes.rb
+++ app/queries/relevant_recipes.rb
@@ -1,7 +1,11 @@
 class RelevantRecipes
 	def call
 		home_ingredients = HomeIngredient.pluck(:name).map{|x|"%#{x.downcase}%"}.join(",")
-		are_all_ingredients_at_home = "SELECT LOWER(name) ~~ ANY('{#{home_ingredients}}') FROM ingredients WHERE ingredients.receipt_id = receipts.id"
-		Receipt.where("true = all(#{are_all_ingredients_at_home})")
+
+		# The bellow subquery is going to return an array of booleans for each ingredient representing if the ingredient can be found at home
+		are_ingredients_at_home = "SELECT LOWER(name) ~~ ANY('{#{home_ingredients}}') FROM ingredients WHERE ingredients.receipt_id = receipts.id"
+
+		# Filtering for those reciped which have all ingredients at home.
+		Receipt.where("true = all(#{are_ingredients_at_home})")
 	end
 end
